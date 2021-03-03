17eb22ecc4ffd270fe43a1e9a3b63291c4bed95f
Jonathan Hernandez <jon.hernandez169@gmail.com>
2021-03-03 16:44:05 -0600

    done with updates

---
+++ client/build/js/index.js
@@ -0,0 +1,11 @@
+// var stickyNav = document.getElementsById("sticky-nav")
+// window.onscroll=function(){
+//     if(window.pageYOffset > 100){
+//       console.log('change')
+//       stickyNav.addClass("position-sticky")
+//     }
+//     else{
+//       console.log('revert')
+//       $('.rnt').removeClass("position-sticky");
+//     }
+// }
