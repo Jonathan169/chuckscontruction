26d25c4b6a8e85df7cefc8fbe9d1e75ed9e2b231
root <root@instance-5.us-central1-a.c.universal-fort-307322.internal>
2021-05-20 17:16:13 +0000

    uploading live copy

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const express = require("express");
-
+const fs = require('fs');
+const https = require('https');
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json())
@@ -19,6 +20,12 @@ app.use(require("./routes"))
 //     res.sendFile(path.join(__dirname, "client/build/index.html"));
 // });
 //listener
+
+//const sslServer = https.createServer({
+//	key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+//	cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+//},app)
+
 app.listen(port,function(){
     console.log("listening on http://localhost:"+port)
 })
